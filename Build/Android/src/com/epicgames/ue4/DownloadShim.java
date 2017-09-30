969ac37f786e8c8f8a9610e3b70d08af69669c29
bobo_rick <abednov@outlook.com>
2017-09-30 17:30:04 +0300

    0.1.4.5.2
    some changes
    build ATC

--- Build/Android/src/com/epicgames/ue4/DownloadShim.java
+++ Build/Android/src/com/epicgames/ue4/DownloadShim.java
@@ -1,7 +1,7 @@
 package com.epicgames.ue4;
 
-import com.YourCompany.phys_3dperson_01.OBBDownloaderService;
-import com.YourCompany.phys_3dperson_01.DownloaderActivity;
+import com.KwarkGames.phys_3dperson_01.OBBDownloaderService;
+import com.KwarkGames.phys_3dperson_01.DownloaderActivity;
 
 
 public class DownloadShim
