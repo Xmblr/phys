7b66b941f731b034fe9f466f0c2d8aaa5021256b
bobo_rick <abednov@outlook.com>
2017-09-30 20:57:23 +0300

    v0.1.4.5.5
    pause menu
    add character
    buiild (pc, atc)
    and other

--- Build/Android/src/com/KwarkGames/phys_3dperson_01/OBBData.java
+++ Build/Android/src/com/KwarkGames/phys_3dperson_01/OBBData.java
@@ -17,7 +17,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-57606059L // the length of the file in bytes
+61315587L // the length of the file in bytes
 )
 };
 };
